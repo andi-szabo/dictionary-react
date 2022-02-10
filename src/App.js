2251011af4bab2e4c14ffbe6de148fd78023c70e
Andrea Szabo <szaboande93@gmail.com>
2022-02-10 19:35:24 +0100

    Added search input field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Open source code by Andrea Szabo</small>
+        </footer>
+      </div>
     </div>
   );
 }
