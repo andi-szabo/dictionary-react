58e63bd2ea3fcacedf2fd72a1c98969396518b5d
Andrea Szabo <szaboande93@gmail.com>
2022-02-12 16:41:36 +0100

    Added the Photo API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     //documentation: https://api.dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    //documentation: https://www.pexels.com/api/documentation/?language=javascript#photos-search
+    let pexelsApiKey =
+      "563492ad6f91700001000001b86cb93a2c7c4d56966e693cd33d5652";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -47,6 +60,7 @@ export default function Dictionary(props) {
           <div className="hint">i.e abstract, art, expressionism...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
